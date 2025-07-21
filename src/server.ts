78a981b5c773adafcd9580f67840ddd5e62ff904
callmeniyu <getniyashere@gmail.com>
2025-07-21 23:08:12 +0530

    complete tour, transfer, blogs

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,8 @@ import app from './app';
 import { env } from './config/env';
 
 const PORT = Number(process.env.PORT) || 3002;
-const HOST = process.env.HOST ||'192.168.33.50' // This allows external connections
 
-app.listen(PORT, HOST, () => {
-    console.log(`Server running on http://${HOST}:${PORT}`)
-    console.log(`Access from mobile: http://${HOST}:${PORT}`)
+app.listen(PORT,  () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`Access from mobile: http://localhost:${PORT}`)
 })
