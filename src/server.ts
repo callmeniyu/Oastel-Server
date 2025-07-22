32fc2ba118c22e19fa38d74987d9340032af0334
callmeniyu <getniyashere@gmail.com>
2025-07-22 11:52:12 +0530

    complete tours

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import app from './app';
-import { env } from './config/env';
 
 const PORT = Number(process.env.PORT) || 3002;
 
 app.listen(PORT,  () => {
     console.log(`Server running on http://localhost:${PORT}`)
-    console.log(`Access from mobile: http://localhost:${PORT}`)
 })
