66d37d1bcca6abc7519bc7309c126de9180805e6
callmeniyu <getniyashere@gmail.com>
2025-08-12 21:13:00 +0530

    fix port deploy on droplet

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import app from './app';
 import connectDB from './config/db';
 
 const PORT = Number(process.env.PORT) || 3002;
-const HOST = process.env.HOST || '192.168.163.50';
+const HOST = process.env.HOST || '10.22.11.50';
 
 // Connect to MongoDB first
 connectDB().then(() => {
     // Only start the server after successful database connection
-    app.listen(PORT, HOST, () => {
+    app.listen(PORT, () => {
         console.log(`Server running on http://${HOST}:${PORT}`)
     });
 }).catch(error => {
